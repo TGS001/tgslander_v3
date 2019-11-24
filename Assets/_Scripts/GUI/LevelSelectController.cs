83b8dfbfe54e261c3656da688d79e6140fdd1ad0
Dan Coburn <dancoburpro@gmail.com>
2019-11-23 23:28:10 -0800

    Adjusted Menus, Split Level Select into 2 Scenes
    Changed menu system to have separate scenes for Level Select and Lander Module handling (now called Lander Workshop), made the menus into prefabs for easier editing and adjusted anchors and positioning of menus to fit more screen sizes. Also moved some art files from prefab folder into art folder, renamed some items to not be for demo anymore.

--- Assets/_Scripts/GUI/LevelSelectController.cs
+++ Assets/_Scripts/GUI/LevelSelectController.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DebugLevelSelectController : MonoBehaviour {
+public class LevelSelectController : MonoBehaviour {
 
 	public void SelectLevel(string name)
     {
         Debug.Log("selected level " + name);
         AsyncLevelLoadController.LoadLevel(name);
     }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
 }
